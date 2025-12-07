81d24b41f9d1704085bcd673fc35169b93af0508
Benoit.G <benoitgaugiran@gmail.com>
2025-12-07 21:15:06 +0100

    Premier commit test

---
+++ config_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+// Boilerplate test to be implemented later, using the config loading function
+func TestConfigLoading(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		config Config
+	}{}
+
+	for _, tt := range tests {
+
+	}
+	t.Log("Config loading test placeholder")
+}
