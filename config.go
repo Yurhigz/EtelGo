66145060af955cda8a6d5ba5fd1c4c2a2a311d43
Yurhigz <benoitgaugiran@gmail.com>
2025-12-08 17:06:41 +0100

    Feature : WIP Yaml parsing

--- config.go
+++ config.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/goccy/go-yaml"
+)
+
 // Config struct which holds the YAML configuration
 // It supports both mandatory and optional fields
 // with appropriate data types.
@@ -12,24 +19,25 @@ type Config struct {
 
 // There are 3 main sections: Input, Processors and Output.
 // Input sections :
-//	- Mandatory :
-// 				Brokers (list of string),
-// 				topic (string)
-// 				consumer_group_id (string)
-// 				format (string)
-// 				schema_registry_url (string) if AVRO or PROTOBUF
-// 				workers (int) - default 1
 //
-//  - Optional() :
-//				offset_reset (string) - default "latest"
-// 				enable_auto_commit (bool) - default false
-// 				auto_commit_interval (int in s) - default 5s
-// 				partitions (list of int)
-// 				min_bytes (int)
-// 				max_bytes (int)
-// 				max_wait_time (int in ms)
-// 				session_timeout (int in ms)
-// 				heartbeat_interval (int in s)
+//   - Mandatory :
+//     Brokers (list of string),
+//     topic (string)
+//     consumer_group_id (string)
+//     format (string)
+//     schema_registry_url (string) if AVRO or PROTOBUF
+//     workers (int) - default 1
+//
+//   - Optional() :
+//     offset_reset (string) - default "latest"
+//     enable_auto_commit (bool) - default false
+//     auto_commit_interval (int in s) - default 5s
+//     partitions (list of int)
+//     min_bytes (int)
+//     max_bytes (int)
+//     max_wait_time (int in ms)
+//     session_timeout (int in ms)
+//     heartbeat_interval (int in s)
 type InputConfig struct {
 	// Mandatory fields
 	Brokers        []string `yaml:"brokers"`
@@ -42,20 +50,20 @@ type InputConfig struct {
 	// Optional fields
 	Offset_reset         string `yaml:"offset_reset,omitempty"`
 	Enable_auto_commit   bool   `yaml:"enable_auto_commit,omitempty"`
-	Auto_commit_interval int    `yaml:"auto_commit_interval,omitempty"`
+	Auto_commit_interval string `yaml:"auto_commit_interval,omitempty"`
 	Partitions           []int  `yaml:"partitions,omitempty"`
 	Min_bytes            int    `yaml:"min_bytes,omitempty"`
 	Max_bytes            int    `yaml:"max_bytes,omitempty"`
 	Max_wait_time        int    `yaml:"max_wait_time,omitempty"`
-	Session_timeout      int    `yaml:"session_timeout,omitempty"`
-	Heartbeat_interval   int    `yaml:"heartbeat_interval,omitempty"`
+	Session_timeout      string `yaml:"session_timeout,omitempty"`
+	Heartbeat_interval   string `yaml:"heartbeat_interval,omitempty"`
 }
 
 // Processors section :
-//	- Mandatory :
-//			- There is no mandatory field in processors section
-//	- Optional :
-// 		- type (string) - "filter", "transform", "enrich" (examples)
+//   - Mandatory :
+//   - There is no mandatory field in processors section
+//   - Optional :
+//   - type (string) - "filter", "transform", "enrich" (examples)
 type ProcessorConfig struct {
 }
 
@@ -68,15 +76,15 @@ type ProcessorConfig struct {
 //		format (string)
 //		schema_registry_url (string) if AVRO or PROTOBUF
 
-//	- Optional :
-//		partitions (list of int)
-//		batch_size (int) - default 2000
-//		compression (string) - "none", "gzip", "snappy", "lz4", "zstd" - default "none"
-//		auto_create_topic (bool) - default false
+//   - Optional :
+//     partitions (list of int)
+//     batch_size (int) - default 2000
+//     compression (string) - "none", "gzip", "snappy", "lz4", "zstd" - default "none"
+//     auto_create_topic (bool) - default false
 //
-//		request_timeout (int in s) - default 30s
-//		retry_backoff (int in s) - default 2s
-//		max_retries (int) - default 3
+//     request_timeout (int in s) - default 30s
+//     retry_backoff (int in s) - default 2s
+//     max_retries (int) - default 3
 type OutputConfig struct {
 	// Mandatory fields
 	Type           string   `yaml:"type"`
@@ -91,14 +99,41 @@ type OutputConfig struct {
 	Batch_size        int    `yaml:"batch_size,omitempty"`
 	Compression       string `yaml:"compression,omitempty"`
 	Auto_create_topic bool   `yaml:"auto_create_topic,omitempty"`
-	Request_timeout   int    `yaml:"request_timeout,omitempty"`
-	Retry_backoff     int    `yaml:"retry_backoff,omitempty"`
+	Request_timeout   string `yaml:"request_timeout,omitempty"`
+	Retry_backoff     string `yaml:"retry_backoff,omitempty"`
 	Max_retries       int    `yaml:"max_retries,omitempty"`
 }
 
 // Yaml Parsing function to load configuration from a YAML file
 // It reads the file, parses the YAML content, and populates the Config struct
 
-func LoadConfigFromYAML(filePath string) (Config, error) {
+func LoadConfig(filePath string, logger *slog.Logger) (*Config, error) {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &Config{}
+
+	err = yaml.Unmarshal(content, cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+
+	// panic("Not implemented yet")
+}
+
+func IsInputMandatory(input InputConfig, logger *slog.Logger) error {
+	panic("Not implemented yet")
+}
+
+func IsOutputMandatory(output OutputConfig, logger *slog.Logger) error {
+	panic("Not implemented yet")
+}
+
+func IsProcessorMandatory(processor ProcessorConfig, logger *slog.Logger) error {
 	panic("Not implemented yet")
 }
