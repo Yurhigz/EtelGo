a461c3ff53c1958f4553f8e565d064d61f706fa1
Yurhigz <benoitgaugiran@gmail.com>
2025-12-09 22:52:55 +0100

    Feature : Validation function

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 
@@ -15,6 +16,22 @@ type Config struct {
 	Input     InputConfig
 	Processor ProcessorConfig
 	Output    OutputConfig
+}
+
+type Format string
+
+const (
+	FormatJSON   Format = "json"
+	FormatAvro   Format = "avro"
+	FormatProto  Format = "protobuf"
+	FormatString Format = "string"
+)
+
+var ValidFormats = map[Format]bool{
+	FormatJSON:   true,
+	FormatAvro:   true,
+	FormatProto:  true,
+	FormatString: true,
 }
 
 // There are 3 main sections: Input, Processors and Output.
@@ -88,7 +105,7 @@ type ProcessorConfig struct {
 type OutputConfig struct {
 	// Mandatory fields
 	Type           string   `yaml:"type"`
-	Brokers        []string `yaml:"broker"`
+	Brokers        []string `yaml:"brokers"`
 	Topic          string   `yaml:"topic"`
 	Workers        int      `yaml:"workers"`
 	Format         string   `yaml:"format"`
@@ -107,6 +124,85 @@ type OutputConfig struct {
 // Yaml Parsing function to load configuration from a YAML file
 // It reads the file, parses the YAML content, and populates the Config struct
 
+type Validator interface {
+	Validate(logger *slog.Logger) error
+}
+
+func (ic *InputConfig) Validate(logger *slog.Logger) error {
+	logger.Debug("Validating InputConfig", "topic", ic.Topic)
+	if len(ic.Brokers) == 0 {
+		logger.Error("InputConfig validation failed: Brokers is required and cannot be empty")
+		return fmt.Errorf("brokers is required and cannot be empty")
+	}
+	if ic.Topic == "" {
+		logger.Error("InputConfig validation failed: Topic is required and cannot be empty")
+		return fmt.Errorf("topic is required and cannot be empty")
+	}
+
+	if ic.ConsumerGroup == "" {
+		logger.Warn("ConsumerGroup has not been provided, using default 'default-group'")
+		ic.ConsumerGroup = "default-group"
+	}
+
+	if !ValidFormats[Format(ic.Format)] {
+		logger.Error("InputConfig validation failed: Unsupported format", "format", ic.Format)
+		return fmt.Errorf("unsupported format: %s", ic.Format)
+	}
+
+	if (ic.Format == "avro" || ic.Format == "protobuf") && ic.SchemaRegistry == "" {
+		logger.Error("InputConfig validation failed: schema_registry_url is required for AVRO and PROTOBUF formats")
+		return fmt.Errorf("schema_registry_url is required for AVRO and PROTOBUF formats")
+	}
+
+	if ic.Workers <= 0 {
+		logger.Warn("Workers not set or invalid, defaulting to 1")
+		ic.Workers = 1
+	}
+
+	logger.Info("InputConfig validation successful")
+	return nil
+}
+
+func (oc *OutputConfig) Validate(logger *slog.Logger) error {
+	logger.Debug("Validating OutputConfig", "topic", oc.Topic)
+	if oc.Type != "kafka" {
+		logger.Error("OutputConfig validation failed: Unsupported output type", "type", oc.Type)
+		return fmt.Errorf("unsupported output type: %s", oc.Type)
+	}
+
+	if len(oc.Brokers) == 0 {
+		logger.Error("OutputConfig validation failed: Brokers is required and cannot be empty")
+		return fmt.Errorf("brokers is required and cannot be empty")
+	}
+
+	if oc.Topic == "" {
+		logger.Error("OutputConfig validation failed: Topic is required and cannot be empty")
+		return fmt.Errorf("topic is required and cannot be empty")
+	}
+
+	if oc.Workers <= 0 {
+		logger.Warn("Workers not set or invalid, defaulting to 1")
+		oc.Workers = 1
+	}
+
+	if !ValidFormats[Format(oc.Format)] {
+		logger.Error("OutputConfig validation failed: Unsupported format", "format", oc.Format)
+		return fmt.Errorf("unsupported format: %s", oc.Format)
+	}
+
+	if (oc.Format == "avro" || oc.Format == "protobuf") && oc.SchemaRegistry == "" {
+		logger.Error("OutputConfig validation failed: schema_registry_url is required for AVRO and PROTOBUF formats")
+		return fmt.Errorf("schema_registry_url is required for AVRO and PROTOBUF formats")
+	}
+
+	logger.Info("InputConfig validation successful")
+	return nil
+}
+
+func (pc *ProcessorConfig) Validate(logger *slog.Logger) error {
+	return nil
+}
+
 func LoadConfig(filePath string, logger *slog.Logger) (*Config, error) {
 	content, err := os.ReadFile(filePath)
 	if err != nil {
@@ -124,16 +220,4 @@ func LoadConfig(filePath string, logger *slog.Logger) (*Config, error) {
 	return cfg, nil
 
 	// panic("Not implemented yet")
-}
-
-func IsInputMandatory(input InputConfig, logger *slog.Logger) error {
-	panic("Not implemented yet")
-}
-
-func IsOutputMandatory(output OutputConfig, logger *slog.Logger) error {
-	panic("Not implemented yet")
-}
-
-func IsProcessorMandatory(processor ProcessorConfig, logger *slog.Logger) error {
-	panic("Not implemented yet")
 }
