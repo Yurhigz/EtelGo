1a6d6a4bad155e5bbd0a7c62e3ad363cb45f04ab
Benoit <benoitgaugiran@gmail.com>
2025-12-19 23:25:55 +0100

    Sprint org

--- sources.go
+++ sources.go
@@ -1,1 +1,8 @@
 package main
+
+// Implémentation d'un lecteur Kafka avec la librairie Franz
+// Il faudra mettre en place un worker pool pour paraléliser au maximum
+
+func KafkaReader() {
+	panic("Not implemented")
+}
