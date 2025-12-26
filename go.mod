a618c89825b093a53e002f7e039913cff135829f
Benoit <benoitgaugiran@gmail.com>
2025-12-26 17:08:52 +0100

    Package added + Kafka client

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module etelgo
 
-go 1.23.0
+go 1.24.0
 
-require github.com/goccy/go-yaml v1.19.0 // indirect
+toolchain go1.24.11
+
+require (
+	github.com/goccy/go-yaml v1.19.0 // indirect
+	github.com/twmb/franz-go v1.20.6 // indirect
+)
--- sources.go
+++ sources.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"log/slog"
+
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
 // Implémentation d'un lecteur Kafka avec la librairie Franz
 // Il faudra mettre en place un worker pool pour paraléliser au maximum
 
-func KafkaReader() {
+func KafkaReader(brokers []string, topic string, groupID string, logger *slog.Logger) {
+	cl, err := kgo.NewClient(
+		kgo.SeedBrokers(brokers...),
+		kgo.ConsumerGroup(groupID),
+		kgo.ConsumeTopics(topic),
+	)
+	if err != nil {
+		logger.Error("failed to create Kafka client", "error", err)
+	}
+
+	defer cl.Close()
 	panic("Not implemented")
 }
